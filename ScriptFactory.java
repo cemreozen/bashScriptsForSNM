f9c3073914b8f0d65d1dc7a39b5982fb81033ce4
cemreozen <152500554+cemreozen@users.noreply.github.com>
2025-04-10 15:37:37 +0200

    Add files via upload
    java files for the script generator

---
+++ ScriptFactory.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class ScriptFactory {
+    private FileOutputStream fos;
+
+
+    public ScriptFactory(File infoSheet) {
+        //extract info from info sheet and assign
+    }
+
+
+
+}
